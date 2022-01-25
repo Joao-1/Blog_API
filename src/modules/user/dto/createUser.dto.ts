19802c36f417e21dc1d15c38fa15d4ae8c969fd8
João Vitor Martins Neto <joaovitormartinsneto9@gmail.com>
2022-01-25 17:44:49 -0300

    More validations

--- src/modules/user/dto/createUser.dto.ts
+++ src/modules/user/dto/createUser.dto.ts
@@ -1,25 +1,57 @@
-import { IsBooleanString, IsEmail, IsNotEmpty, IsOptional, IsString, MinLength, ValidateIf } from "class-validator";
+/* eslint-disable max-classes-per-file */
+import { Type } from "class-transformer";
+import {
+	IsBooleanString,
+	IsEmail,
+	IsIn,
+	IsNotEmpty,
+	IsString,
+	MinLength,
+	ValidateIf,
+	ValidateNested,
+} from "class-validator";
+
+class OAuthDTO {
+	@IsNotEmpty()
+	@IsBooleanString()
+	value: boolean;
+
+	@ValidateIf((p) => p.value === "true")
+	@IsIn(["google", "facebook", "twitter"])
+	@IsNotEmpty()
+	type: "google" | "facebook" | "twitter";
+}
 
 export default class CreateUserDtoBody {
-	@ValidateIf((p) => p.withOAuth === "false")
+	@ValidateIf((p) => p.isOAuth && p.isOAuth.value === "false")
 	@IsNotEmpty()
 	username: string;
 
-	@ValidateIf((p) => p.withOAuth === "false")
+	@ValidateIf((p) => p.isOAuth && p.isOAuth.value === "false")
 	@MinLength(6)
 	@IsNotEmpty()
 	password: string;
 
-	@ValidateIf((p) => p.withOAuth === "false")
+	@ValidateIf((p) => p.isOAuth && p.isOAuth.value === "false")
 	@IsNotEmpty()
 	@IsString()
 	@IsEmail()
 	email: string;
 
 	@IsNotEmpty()
-	@IsBooleanString()
-	withOAuth: string;
+	@ValidateNested()
+	@Type(() => OAuthDTO)
+	isOAuth: OAuthDTO;
 
-	@IsOptional()
+	@ValidateIf((p) => p.isOAuth && p.isOAuth.value === "true" && p.isOAuth.type === "google")
+	@IsNotEmpty()
 	googleId: string;
+
+	@ValidateIf((p) => p.isOAuth && p.isOAuth.value === "true" && p.isOAuth.type === "facebook")
+	@IsNotEmpty()
+	facebookId: string;
+
+	@ValidateIf((p) => p.isOAuth && p.isOAuth.value === "true" && p.isOAuth.type === "twitter")
+	@IsNotEmpty()
+	twiiterId: string;
 }
